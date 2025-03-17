2401900ff9f480457734c68d7681d6ef76bc1b2c
Darsh04-14 <darshpatelcr7@gmail.com>
2025-03-17 05:55:13 -0400

    year formatting fix

--- popup.js
+++ popup.js
@@ -8,7 +8,7 @@ const sendMessageToContent = (message) => {
 const getFormattedDate = () => {
   const now = new Date();
     
-  const yy = String(now.getFullYear()).slice(-2);
+  const yy = String(now.getFullYear());
   const mm = String(now.getMonth() + 1).padStart(2, '0');
   const dd = String(now.getDate()).padStart(2, '0');
   
